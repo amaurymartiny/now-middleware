f92311b9a0279f74ea4c1502038097db5a5ed3aa
Amaury <1293565+amaurym@users.noreply.github.com>
2021-02-22 11:04:26 +0100

    chore: Update names and links (#86)
    * chore: Update names and links
    
    * Run yarn install

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,37 +2,37 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-### [0.2.1](https://github.com/amaurymartiny/now-middleware/compare/v0.2.0...v0.2.1) (2020-09-20)
+### [0.2.1](https://github.com/amaurym/now-middleware/compare/v0.2.0...v0.2.1) (2020-09-20)
 
 
 ### Bug Fixes
 
-* move `express` and `@types/express` to devDependencies ([#67](https://github.com/amaurymartiny/now-middleware/issues/67)) ([5a0231e](https://github.com/amaurymartiny/now-middleware/commit/5a0231e2cb63b3539b3c6168b6a3c53861b30508))
+* move `express` and `@types/express` to devDependencies ([#67](https://github.com/amaurym/now-middleware/issues/67)) ([5a0231e](https://github.com/amaurym/now-middleware/commit/5a0231e2cb63b3539b3c6168b6a3c53861b30508))
 
-## [0.2.0](https://github.com/amaurymartiny/now-middleware/compare/v0.1.2...v0.2.0) (2020-05-30)
+## [0.2.0](https://github.com/amaurym/now-middleware/compare/v0.1.2...v0.2.0) (2020-05-30)
 
 
 ### ⚠ BREAKING CHANGES
 
 * This package's peer dependency has been changed from `@now/node` to `@vercel/node`.
 
-### [0.1.2](https://github.com/amaurymartiny/now-middleware/compare/v0.1.1...v0.1.2) (2020-05-05)
+### [0.1.2](https://github.com/amaurym/now-middleware/compare/v0.1.1...v0.1.2) (2020-05-05)
 
 
 ### Bug Fixes
 
-* Allow error middlewares ([#26](https://github.com/amaurymartiny/now-middleware/issues/26)) ([55a12a0](https://github.com/amaurymartiny/now-middleware/commit/55a12a08f744420c4b34861eff55e3ba883423e8))
+* Allow error middlewares ([#26](https://github.com/amaurym/now-middleware/issues/26)) ([55a12a0](https://github.com/amaurym/now-middleware/commit/55a12a08f744420c4b34861eff55e3ba883423e8))
 
-### [0.1.1](https://github.com/amaurymartiny/now-middleware/compare/v0.1.0...v0.1.1) (2020-02-15)
+### [0.1.1](https://github.com/amaurym/now-middleware/compare/v0.1.0...v0.1.1) (2020-02-15)
 
 
 ### Bug Fixes
 
-* Export chain.ts from index.ts ([#3](https://github.com/amaurymartiny/now-middleware/issues/3)) ([05581e8](https://github.com/amaurymartiny/now-middleware/commit/05581e8d3c7b8a188e4ea6accf550c5577357f24))
+* Export chain.ts from index.ts ([#3](https://github.com/amaurym/now-middleware/issues/3)) ([05581e8](https://github.com/amaurym/now-middleware/commit/05581e8d3c7b8a188e4ea6accf550c5577357f24))
 
 ## 0.1.0 (2020-02-14)
 
 
 ### Features
 
-* Setup first version ([a0d2824](https://github.com/amaurymartiny/now-middleware/commit/a0d28247fe608c99b3b2ac5a91fe610d46186dc7))
+* Setup first version ([a0d2824](https://github.com/amaurym/now-middleware/commit/a0d28247fe608c99b3b2ac5a91fe610d46186dc7))
--- src/chain.spec.ts
+++ src/chain.spec.ts
@@ -1,5 +1,5 @@
-// @amaurymartiny/now-middleware
-// Copyright (C) 2020 Amaury Martiny
+// @amaurym/now-middleware
+// Copyright (C) 2020-2021 Amaury
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,112 +20,116 @@ import type { NextFunction, Request, Response } from 'express';
 import { chain } from './chain';
 
 describe('chain', () => {
-  it('should work', () => {
-    // A dummy Express middleware
-    function dummyMiddleware(
-      req: Request,
-      _res: Response,
-      next: NextFunction
-    ): void {
-      req.headers.foo = 'foo';
-      next();
-    }
+	it('should work', () => {
+		// A dummy Express middleware
+		function dummyMiddleware(
+			req: Request,
+			_res: Response,
+			next: NextFunction
+		): void {
+			req.headers.foo = 'foo';
+			next();
+		}
 
-    // A dummy ZEIT now handler
-    function dummyHandler(req: NowRequest, res: NowResponse): void {
-      expect(req.headers.foo).toBe('foo');
+		// A dummy ZEIT now handler
+		function dummyHandler(req: NowRequest, res: NowResponse): void {
+			expect(req.headers.foo).toBe('foo');
 
-      res.send('bar');
-    }
+			res.send('bar');
+		}
 
-    const req = { headers: {} } as NowRequest;
-    const res = ({
-      send: jest.fn(),
-    } as unknown) as NowResponse;
+		const req = { headers: {} } as NowRequest;
+		const res = ({
+			send: jest.fn(),
+		} as unknown) as NowResponse;
 
-    chain(dummyMiddleware)(dummyHandler)(req, res);
-    expect(res.send).toHaveBeenCalledWith('bar');
-  });
+		chain(dummyMiddleware)(dummyHandler)(req, res) as void;
+		expect(res.send).toHaveBeenCalledWith('bar');
+	});
 
-  it('should work deal with error sended as param of next', () => {
-    // A dummy Express middleware
-    function errorMiddleware(
-      _req: Request,
-      _res: Response,
-      next: NextFunction
-    ): void {
-      next(new Error('errorMiddleware'));
-    }
+	it('should work deal with error sended as param of next', () => {
+		// A dummy Express middleware
+		function errorMiddleware(
+			_req: Request,
+			_res: Response,
+			next: NextFunction
+		): void {
+			next(new Error('errorMiddleware'));
+		}
 
-    function errorCatchMiddleware(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      err: any,
-      _req: Request,
-      res: Response,
-      next: NextFunction
-    ): void {
-      if (err) {
-        res.send(err.message);
-        return;
-      }
-      next();
-    }
+		function errorCatchMiddleware(
+			err: Error,
+			_req: Request,
+			res: Response,
+			next: NextFunction
+		): void {
+			if (err) {
+				res.send(err.message);
+				return;
+			}
+			next();
+		}
 
-    // A dummy ZEIT now handler
-    function dummyHandler(req: NowRequest, res: NowResponse): void {
-      expect(req.headers.foo).toBe('foo');
+		// A dummy ZEIT now handler
+		function dummyHandler(req: NowRequest, res: NowResponse): void {
+			expect(req.headers.foo).toBe('foo');
 
-      res.send('bar');
-    }
+			res.send('bar');
+		}
 
-    const req = { headers: {} } as NowRequest;
-    const res = ({
-      send: jest.fn(),
-    } as unknown) as NowResponse;
+		const req = { headers: {} } as NowRequest;
+		const res = ({
+			send: jest.fn(),
+		} as unknown) as NowResponse;
 
-    chain(errorMiddleware, errorCatchMiddleware)(dummyHandler)(req, res);
-    expect(res.send).toHaveBeenCalledWith('errorMiddleware');
-    expect(res.send).not.toHaveBeenCalledWith('bar');
-  });
+		chain(errorMiddleware, errorCatchMiddleware)(dummyHandler)(
+			req,
+			res
+		) as void;
+		expect(res.send).toHaveBeenCalledWith('errorMiddleware');
+		expect(res.send).not.toHaveBeenCalledWith('bar');
+	});
 
-  it('should work deal with error when thrown in the middleware', () => {
-    // A dummy Express middleware
-    function errorThrowMiddleware(
-      _req: Request,
-      _res: Response,
-      _next: NextFunction
-    ): void {
-      throw new Error('errorThrowMiddleware');
-    }
+	it('should work deal with error when thrown in the middleware', () => {
+		// A dummy Express middleware
+		function errorThrowMiddleware(
+			_req: Request,
+			_res: Response,
+			_next: NextFunction
+		): void {
+			throw new Error('errorThrowMiddleware');
+		}
 
-    function errorCatchMiddleware(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      err: any,
-      _req: Request,
-      res: Response,
-      next: NextFunction
-    ): void {
-      if (err) {
-        res.send(err.message);
-        return;
-      }
-      next();
-    }
+		function errorCatchMiddleware(
+			err: Error,
+			_req: Request,
+			res: Response,
+			next: NextFunction
+		): void {
+			if (err) {
+				res.send(err.message);
+				return;
+			}
+			next();
+		}
 
-    // A dummy ZEIT now handler
-    function dummyHandler(req: NowRequest, res: NowResponse): void {
-      expect(req.headers.foo).toBe('foo');
+		// A dummy ZEIT now handler
+		function dummyHandler(req: NowRequest, res: NowResponse): void {
+			expect(req.headers.foo).toBe('foo');
 
-      res.send('bar');
-    }
+			res.send('bar');
+		}
 
-    const req = { headers: {} } as NowRequest;
-    const res = ({
-      send: jest.fn(),
-    } as unknown) as NowResponse;
+		const req = { headers: {} } as NowRequest;
+		const res = ({
+			send: jest.fn(),
+		} as unknown) as NowResponse;
 
-    chain(errorThrowMiddleware, errorCatchMiddleware)(dummyHandler)(req, res);
-    expect(res.send).toHaveBeenCalledWith('errorThrowMiddleware');
-    expect(res.send).not.toHaveBeenCalledWith('bar');
-  });
+		chain(errorThrowMiddleware, errorCatchMiddleware)(dummyHandler)(
+			req,
+			res
+		) as void;
+		expect(res.send).toHaveBeenCalledWith('errorThrowMiddleware');
+		expect(res.send).not.toHaveBeenCalledWith('bar');
+	});
 });
